0ac16b6330dda05ba303528495c04f80313e751f
Charlys <charlysjerfeson@gmail.com>
2022-10-14 09:24:31 -0300

    .

--- C/Lista de exercício em C/Ufersa/LISTA VIII/Q05.c
+++ C/Lista de exercício em C/Ufersa/LISTA VIII/Q05.c
@@ -5,6 +5,19 @@
 /* 5. Construa uma função, sem retorno, que recebe um valor inteiro por parâmetro e
 informa se ele é, ou não, múltiplo de 5. */
 
+int multiplo( int n)
+{
+    if (n%5==0)
+    {
+        printf("Este Nº é múltiplo de 5\n");
+    }
+
+    else
+    {
+        printf("Este Nº não é múltiplo de 5\n");
+    }
+}
+
 int main()
 {
     setlocale(LC_ALL, " ");
@@ -13,6 +26,8 @@ int main()
 
     printf("Digite um número\n");
     scanf(" %d", &n);
+
+    multiplo(n);
 
     system("pause");
     return (0);
