eee027599c9106864946c519ef4f30a0b9beade0
Tcharlys <charlysjerfeson@gmail.com>
2023-01-20 09:39:08 -0300

    respondendo

--- UFERSA/Estrutura de Dados I/Unidade I/Teste/Q04.c
+++ UFERSA/Estrutura de Dados I/Unidade I/Teste/Q04.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 
+int somatorio( int n1, int n2)
+{
+    int n = 0, i;
+
+    for ( i = 0; i < n2; i++)
+    {
+        if ((i > n1) && (i <n2))
+        {
+            i+=i;
+        }
+        
+    }
+    
+    return (i);
+}
+
 int main()
 {
-    
-        
+    int n1 = 2, n2 = 6, soma;
+
+    soma = somatorio( n1, n2);
+
+    printf("Somatório -> %d\n", soma);
+
     return 0;
 }
